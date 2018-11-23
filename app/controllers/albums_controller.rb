c1a1312ab387e38d355d2f05062eb8248eae5282
Code <code@scotts-mbp.attlocal.net>
2018-11-23 08:21:36 -0500

    album controller and image responsiveness

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,14 +3,45 @@ class AlbumsController < ApplicationController
   end
 
   def new
+    @album = Album.new
+    @artist = Artist.find(params['artist'])
   end
 
   def create
+    @album = Album.new album_params
+    if @album.save
+      redirect_to artist_path(@album.artist)
+    else
+      render :new
+    end
   end
 
   def edit
+    @album = Album.find(params['id'])
+  end
+
+  def update
+    @album = Album.find(params['id'])
+    binding.pry
+    if @album.update_attributes(album_params) && attach_image
+      flash[:success] = "Album updated"
+      redirect_to @album.artist
+    else
+      render 'edit'
+    end
   end
 
   def delete
   end
+end
+
+private
+
+def attach_image
+  @album.image.attach(album_params[:image]) if album_params[:image].present?
+  true
+end
+
+def album_params
+  params.require(:album).permit(:title, :artist_id, :image)
 end
